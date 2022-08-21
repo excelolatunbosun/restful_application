c8ff750b06e17d35532473a9e27f08e4eafe2786
excelolatunbosun <excellenceolatunbosun@betpawa.com>
2022-08-21 09:59:52 +0100

    included devtools dependencies

--- src/main/java/com/codewithexcel/application/controller/HelloController.java
+++ src/main/java/com/codewithexcel/application/controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.codewithexcel.application.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld(){
+    @GetMapping("/")
+    public String helloWorld() {
         return "Welcome to Excel's Application";
     }
 }
